621d7423dab1087d30043b4dc47f5ad347f1dfab
Rick <rickviwaarden@gmail.com>
2025-01-23 21:51:25 +0100

    changes

---
+++ research/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // fadeIn animatie voor container
+    $('.container').css('opacity', '1');
+
+    // slideDown animatie voor header en footer
+    $('header, footer').each(function (index) {
+        $(this).css('opacity', '1');
+        $(this).css('animation-delay', (index + 1) * 0.5 + 's');
+    });
+
+    // slideUp animatie voor elke sectie
+    $('section').each(function (index) {
+        $(this).css('opacity', '1');
+        $(this).css('animation-delay', (index + 1) * 0.5 + 's');
+    });
+});
